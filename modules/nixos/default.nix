de8ecda0d9663c4035341d5b22005649253260a0
Ludw <61082023+cooukiez@users.noreply.github.com>
2025-12-30 00:09:18 +0100

    update

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,8 +1,5 @@
 # add reusable nixos modules to this directory, on their own file (https://nixos.wiki/wiki/Module)
 # these should be stuff you would like to share with others, not your personal configurations
-{ pkgs, ... }: {
+{
   # list your module files here
-  common = import ./common;
-  desktop = import ./desktop;
-  programs = import ./programs;
 }
