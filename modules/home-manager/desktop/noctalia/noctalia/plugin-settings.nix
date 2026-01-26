fdc8dc7b21dd16f70c49ee41d4ba42c850825d28
Ludw <ludwig-geyer@web.de>
2026-01-27 00:58:30 +0100

    updated noctalia config

---
+++ modules/home-manager/desktop/noctalia/noctalia/plugin-settings.nix
@@ -0,0 +1,43 @@
+{
+  catwalk = {
+    minimumThreshold = 20;
+    hideBackground = false;
+  };
+
+  timer = {
+    compactMode = false;
+  };
+
+  world-clock = {
+    timezones = [
+      {
+        name = "New York";
+        timezone = "America/New_York";
+        enabled = true;
+      }
+      {
+        name = "London";
+        timezone = "Europe/London";
+        enabled = true;
+      }
+      {
+        name = "Tokyo";
+        timezone = "Asia/Tokyo";
+        enabled = true;
+      }
+      {
+        name = "Nova York";
+        timezone = "America/New_York";
+        enabled = true;
+      }
+      {
+        name = "Nova York";
+        timezone = "America/New_York";
+        enabled = true;
+      }
+    ];
+
+    rotationInterval = 5000;
+    timeFormat = "HH:mm";
+  };
+}
---
+++ modules/home-manager/desktop/noctalia/noctalia/plugins.nix
@@ -0,0 +1,62 @@
+{
+  sources = [
+    {
+      enabled = true;
+      name = "Noctalia Plugins";
+      url = "https://github.com/noctalia-dev/noctalia-plugins";
+    }
+  ];
+
+  states = {
+    catwalk = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    fancy-audiovisualizer = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    kaomoji-provider = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    keybind-cheatsheet = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    network-indicator = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    privacy-indicator = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    screen-recorder = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    screenshot = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    timer = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    translator = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    update-count = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+    world-clock = {
+      enabled = true;
+      sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+    };
+  };
+
+  version = 1;
+}
