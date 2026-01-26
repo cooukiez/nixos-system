cbc942c8099d12f9a216b4938a24d22d431ffceb
Ludw <ludwig-geyer@web.de>
2026-01-26 17:35:20 +0100

    add debugging nvim

--- modules/home-manager/programs/nvim/default.nix
+++ modules/home-manager/programs/nvim/default.nix
@@ -1,7 +1,22 @@
 { config, pkgs, ... }:
+let
+  # read all files in the current directory
+  files = builtins.readDir ./.;
+
+  # filter out default.nix and non nix files
+  nixFiles = builtins.filter (name: name != "default.nix" && builtins.match ".*\\.nix" name != null) (
+    builtins.attrNames files
+  );
+
+  # create a list of import statements
+  fileImports = map (name: ./. + "/${name}") nixFiles;
+in
 {
   programs.nixvim = {
     enable = true;
+    version.enableNixpkgsReleaseCheck = false;
+
+    imports = fileImports;
 
     colorschemes.onedark.enable = true;
   };
