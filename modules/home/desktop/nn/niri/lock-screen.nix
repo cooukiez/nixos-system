f7a736463fa2b46fb80ae41ff217a3e3a7d96489
Ludw <ludwig-geyer@web.de>
2026-02-25 00:01:22 +0100

    add lockscreen

---
+++ modules/home/desktop/nn/niri/lock-screen.nix
@@ -0,0 +1,87 @@
+{ config, lib, ... }:
+
+{
+  programs.hyprlock = {
+    enable = true;
+
+    settings = {
+      general = {
+        hide_cursor = true;
+        ignore_empty_input = true;
+      };
+
+      auth = {
+        fingerprint.enabled = true;
+      };
+
+      background = [
+        {
+          monitor = "";
+          path = ./lockscreen;
+          blur_passes = 0;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
+        }
+      ];
+
+      label = [
+        # Time
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
+          color = "rgba(216, 222, 233, 0.7)";
+          font_size = 160;
+          font_family = "steelfish outline regular";
+          position = "0, 370";
+          halign = "center";
+          valign = "center";
+        }
+        # Day-Month-Date
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
+          color = "rgba(216, 222, 233, 0.7)";
+          font_size = 28;
+          font_family = "SF Pro Display Bold";
+          position = "0, 490";
+          halign = "center";
+          valign = "center";
+        }
+        # USER
+        {
+          monitor = "";
+          text = "    $USER";
+          color = "rgba(216, 222, 233, 0.80)";
+          font_size = 18;
+          font_family = "SF Pro Display Bold";
+          position = "0, -180";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      input-field = [
+        {
+          monitor = "";
+          size = "300, 60";
+          outline_thickness = 2;
+          dots_size = 0.2;
+          dots_spacing = 0.2;
+          dots_center = true;
+          outer_color = "rgba(255, 255, 255, 0)";
+          inner_color = "rgba(255, 255, 255, 0.1)";
+          font_color = "rgb(200, 200, 200)";
+          fade_on_empty = false;
+          font_family = "SF Pro Display Bold";
+          placeholder_text = ''<i><span foreground="##ffffff99">🔒 Enter Pass</span></i>'';
+          hide_input = false;
+          position = "0, -250";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
+  };
+}
