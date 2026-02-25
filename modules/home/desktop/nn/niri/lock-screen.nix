ddce989039532af116b1cfa39ab25a5e6c1db744
Ludw <ludwig-geyer@web.de>
2026-02-25 12:33:05 +0100

    fix 4k scaling for hyprlock

--- modules/home/desktop/nn/niri/lock-screen.nix
+++ modules/home/desktop/nn/niri/lock-screen.nix
@@ -1,5 +1,12 @@
-{ config, lib, ... }:
-
+{
+  config,
+  lib,
+  ...
+}:
+let
+  primaryMonitor = "eDP-1";
+  displayScale = 2;
+in
 {
   programs.hyprlock = {
     enable = true;
@@ -8,6 +15,7 @@
       general = {
         hide_cursor = true;
         ignore_empty_input = true;
+        fractional_scaling = 2;
       };
 
       auth = {
@@ -16,11 +24,15 @@
 
       background = [
         {
-          monitor = "";
+          monitor = primaryMonitor;
+
           path = "${./lockscreen.jpg}";
+
           blur_passes = 1;
+
           contrast = 0.8916;
           brightness = 0.8172;
+
           vibrancy = 0.1696;
           vibrancy_darkness = 0.0;
         }
@@ -29,34 +41,43 @@
       label = [
         # Time
         {
-          monitor = "";
+          monitor = primaryMonitor;
+
           text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
           color = "rgba(216, 222, 233, 0.7)";
-          font_size = 160;
+
+          font_size = 160 * displayScale;
           font_family = "steelfish outline regular";
-          position = "0, 370";
+
+          position = "${toString (0 * displayScale)}, ${toString (370 * displayScale)}";
           halign = "center";
           valign = "center";
         }
         # Day-Month-Date
         {
-          monitor = "";
+          monitor = primaryMonitor;
+
           text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
           color = "rgba(216, 222, 233, 0.7)";
-          font_size = 28;
+
+          font_size = 28 * displayScale;
           font_family = "SF Pro Display Bold";
-          position = "0, 490";
+
+          position = "${toString (0 * displayScale)}, ${toString (490 * displayScale)}";
           halign = "center";
           valign = "center";
         }
         # USER
         {
-          monitor = "";
+          monitor = primaryMonitor;
+
           text = "Welcome back, $USER!";
           color = "rgba(216, 222, 233, 0.80)";
-          font_size = 18;
+
+          font_size = 18 * displayScale;
           font_family = "SF Pro Display Bold";
-          position = "0, -180";
+
+          position = "${toString (0 * displayScale)}, ${toString (-180 * displayScale)}";
           halign = "center";
           valign = "center";
         }
@@ -64,20 +85,26 @@
 
       input-field = [
         {
-          monitor = "";
-          size = "300, 60";
-          outline_thickness = 2;
+          monitor = primaryMonitor;
+
+          size = "${toString (300 * displayScale)}, ${toString (60 * displayScale)}";
+
           dots_size = 0.2;
           dots_spacing = 0.2;
           dots_center = true;
+
+          outline_thickness = 2;
           outer_color = "rgba(255, 255, 255, 0)";
           inner_color = "rgba(255, 255, 255, 0.1)";
           font_color = "rgb(200, 200, 200)";
+
           fade_on_empty = false;
+          hide_input = false;
+
           font_family = "SF Pro Display Bold";
           placeholder_text = ''<i><span foreground="##ffffff99">Enter Password</span></i>'';
-          hide_input = false;
-          position = "0, -250";
+
+          position = "${toString (0 * displayScale)}, ${toString (-250 * displayScale)}";
           halign = "center";
           valign = "center";
         }
