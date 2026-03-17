dfa7428379871e9eaca71799fc63fcccdb815f52
Ludw <ludwig-geyer@web.de>
2026-03-17 21:16:44 +0100

    update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,15 +10,15 @@
 pkgs:
 let
   # appmenuGtkModule = pkgs.callPackage ./appmenu-gtk-module.nix {};
-  hardcodeTray = pkgs.callPackage ./hardcode-tray.nix { };
-  heidiSql = pkgs.callPackage ./heidisql.nix { };
-  mcMojaveCursorTheme = pkgs.callPackage ./mcmojave-cursor-theme.nix { };
-  steelFishFonts = pkgs.callPackage ./steelfish-fonts.nix { };
+  hardcodeTrayPkg = pkgs.callPackage ./hardcode-tray.nix { };
+  heidisqlPkg = pkgs.callPackage ./heidisql.nix { };
+  mcMojaveCursorThemePkg = pkgs.callPackage ./mcmojave-cursor-theme.nix { };
+  steelFishFontsPkg = pkgs.callPackage ./steelfish-fonts.nix { };
 in
 {
   # appmenu-gtk-module = appmenuGtkModule;
-  hardcode-tray = hardcodeTray;
-  heidisql = heidiSql;
-  mcmojave-cursor-theme = mcMojaveCursorTheme;
-  steelfish-fonts = steelFishFonts;
+  hardcode-tray = hardcodeTrayPkg;
+  heidisql = heidisqlPkg;
+  mcmojave-cursor-theme = mcMojaveCursorThemePkg;
+  steelfish-fonts = steelFishFontsPkg;
 }
