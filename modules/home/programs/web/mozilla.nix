f52b52f95d1f5f7b77ac608afbc0a9b2f0937192
Ludwig <ludwig-geyer@web.de>
2026-04-28 15:56:36 +0200

    browser config united

---
+++ modules/home/programs/web/mozilla.nix
@@ -0,0 +1,85 @@
+{
+  core = {
+    # performance
+    "gfx.webrender.all" = true;
+    "layers.acceleration.force-enabled" = true;
+    "media.hardware-video-decoding.enabled" = true;
+    "network.http.pipelining" = true;
+    "network.http.pipelining.maxrequests" = 8;
+
+    # desktop integration
+    "widget.use-xdg-desktop-portal.file-picker" = 1;
+    "widget.use-xdg-desktop-portal.settings" = 1;
+    "widget.gtk.global-menu.enabled" = true;
+    "widget.gtk.global-menu.wayland.enabled" = true;
+    "widget.gtk.native-context-menus" = false;
+
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  };
+
+  firefoxCore = {
+    # set locale
+    "browser.search.region" = "DE";
+    "browser.search.isUS" = false;
+    "distribution.searchplugins.defaultLocale" = "en-US";
+    "general.useragent.locale" = "en-Us";
+
+    # performance
+    "browser.cache.disk.enable" = false;
+    "browser.cache.memory.enable" = true;
+
+    # networking & local dns
+    "browser.fixup.domainsuffixwhitelist.lan" = true;
+    "browser.fixup.domainsuffixwhitelist.local" = true;
+
+    # sponsors
+    "browser.newtabpage.activity-stream.system.showSponsored" = false;
+    "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+    "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+    "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored" = false;
+    "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+
+    # content blocking
+    "browser.contentblocking.category" = "strict";
+
+    # disable requirement for extension signatures
+    "xpinstall.signatures.required" = false;
+
+    # allow pipewire camera
+    "media.webrtc.camera.allow-pipewire" = true;
+
+    # 0 = blank
+    # 1 = home page
+    # 2 = last visited
+    # 3 = previous session
+
+    "browser.startup.page" = 0;
+    "browser.startup.homepage" = "http://localhost/";
+    "browser.newtabpage.enabled" = false;
+
+    # new tab pinned
+    "browser.newtabpage.pinned" = [ ];
+
+    # default browser checks
+    "browser.shell.checkDefaultBrowser" = false;
+  };
+
+  firefoxExtra = {
+    # hides window buttons
+    "browser.tabs.drawInTitlebar" = true;
+
+    "browser.compactmode.show" = true;
+    "browser.uidensity" = 0;
+  };
+
+  zenExtra = {
+    # restore pins to original URL, not last visited
+    "zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url" = true;
+
+    # show essential pins in all workspaces
+    "zen.workspaces.separate-essentials" = false;
+
+    # restore workspaces / tabs from previous session
+    "zen.workspaces.continue-where-left-off" = true;
+  };
+}
