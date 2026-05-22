fff0d2c25623683997dd81f10c0906da43220168
cooukiez <ludwig-geyer@web.de>
2026-05-22 16:56:22 +0200

    add facter and hardware config to rtp

--- hosts/rtp/hardware-generated.nix
+++ hosts/rtp/hardware-generated.nix
@@ -1,2 +1,67 @@
 {
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+  ];
+
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/47ae29a9-f04a-460e-9159-6eda50cbf45e";
+    fsType = "btrfs";
+    options = ["subvol=root"];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/AA59-D168";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/47ae29a9-f04a-460e-9159-6eda50cbf45e";
+    fsType = "btrfs";
+    options = ["subvol=data"];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/47ae29a9-f04a-460e-9159-6eda50cbf45e";
+    fsType = "btrfs";
+    options = ["subvol=home"];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/47ae29a9-f04a-460e-9159-6eda50cbf45e";
+    fsType = "btrfs";
+    options = ["subvol=nix"];
+  };
+
+  fileSystems."/var" = {
+    device = "/dev/disk/by-uuid/47ae29a9-f04a-460e-9159-6eda50cbf45e";
+    fsType = "btrfs";
+    options = ["subvol=var"];
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/690b5019-ca45-4e7a-97f8-cc2845feec22";}
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
