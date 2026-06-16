18ee557a943776037c9a87608ad5945dd0601863
cooukiez <ludwig-geyer@web.de>
2026-06-16 22:56:07 +0200

    fixes

--- modules/home/programs/packages.nix
+++ modules/home/programs/packages.nix
@@ -9,7 +9,6 @@ created 2026-04-27 by ludw
   # desktop specific
   #
   nn = with pkgs; [
-    /*
     errands
     euphonica
     exhibit
@@ -28,11 +27,9 @@ created 2026-04-27 by ludw
     qalculate-gtk
     quick-lookup
     system-config-printer
-    */
   ];
 
   gnome = with pkgs; [
-    /*
     gnome-calculator
     gnome-calendar
     gnome-clocks
@@ -64,7 +61,6 @@ created 2026-04-27 by ludw
           --set XDG_CURRENT_DESKTOP "GNOME"
       '';
     })
-    */
   ];
 
   kde = with pkgs; [
